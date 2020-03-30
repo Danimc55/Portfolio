eaa3d19b39049c76d3049621d804f8cf43435f92
unknown <danijeltomic2004@gmail.com>
2020-03-30 23:05:04 +0200

    Did some good changes

---
+++ scripts/fullpage.js
@@ -0,0 +1,7 @@
+new fullpage('#fullPage', {
+    autoScrolling: true,
+    navigation: true,
+    navigationTooltips: ['Home', 'Projects', 'Contact'],
+    anchors: ['section1', 'section2', 'seciton3'],
+    showActiveTooltip: true,
+})
